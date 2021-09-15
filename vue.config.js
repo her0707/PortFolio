28950f351b83eccaf80032f564b4580332dd641f
heowonho <her2478@gmail.com>
2021-09-15 23:47:57 +0900

    output dir 수정

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,6 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+  outputDir: "docs"
 };
