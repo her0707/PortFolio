74b4b230a9d90722e7ee4176417f39b1af6a302b
heowonho <her2478@gmail.com>
2021-09-15 23:40:28 +0900

    production 옵션 추가

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   configureWebpack: {
-      resolve: {
-          alias: {
-              '@': path.join(__dirname, 'src/')
-          }
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src/")
       }
-  }
-}
+    }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/portFolio/" : "/"
+};
